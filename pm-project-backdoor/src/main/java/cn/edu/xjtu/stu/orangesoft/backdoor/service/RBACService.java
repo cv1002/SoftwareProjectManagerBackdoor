f8c6e1b3297d94103a88d06e34eea9cd914d15b4
Kuroneko <74940958+Neroya@users.noreply.github.com>
2020-12-25 11:54:08 +0800

    同步RBAC部分

---
+++ pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/service/RBACService.java
@@ -0,0 +1,19 @@
+package cn.edu.xjtu.stu.orangesoft.backdoor.service;
+
+import cn.edu.xjtu.stu.orangesoft.backdoor.mapper.RBACMapper;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Object;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Operation;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.PermissionConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RBACService {
+    @Autowired
+    RBACMapper rbacMapper;
+
+    public boolean CheckPermission(Integer roleID, Object object, Operation operation) {
+        PermissionConfig config = rbacMapper.CheckPermission(roleID, object, operation);
+        return config.getPermission() != 0;
+    }
+}
