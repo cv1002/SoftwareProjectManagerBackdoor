6cb9f4ea49994c3691e3683edd2e774722cbee7a
ucalan <56287993+ucalan@users.noreply.github.com>
2020-12-25 16:17:14 +0800

    RBACService改动
    由于API的设计，我们需要一个参数为userid, userpassword, object, operation的RBACService

--- pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/service/RBACService.java
+++ pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/service/RBACService.java
@@ -1,9 +1,9 @@
 package cn.edu.xjtu.stu.orangesoft.backdoor.service;
 
 import cn.edu.xjtu.stu.orangesoft.backdoor.mapper.RBACMapper;
+import cn.edu.xjtu.stu.orangesoft.backdoor.mapper.UserMapper;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.*;
 import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Object;
-import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Operation;
-import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.PermissionConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,13 @@ import org.springframework.stereotype.Service;
 public class RBACService {
     @Autowired
     RBACMapper rbacMapper;
+    @Autowired
+    UserMapper userMapper;
 
+    public boolean CheckPermission(Integer userID, String userPassword, Object object, Operation operation) {
+        User user = userMapper.GetUserByIDAndPassword(userID, userPassword);
+        return CheckPermission(user.getRoleID(), object, operation);
+    }
     public boolean CheckPermission(Integer roleID, Object object, Operation operation) {
         PermissionConfig config = rbacMapper.CheckPermission(roleID, object, operation);
         return config.getPermission() != 0;
