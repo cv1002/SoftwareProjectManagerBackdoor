097a214e20db1687c292ebce4c1c0e52cd181fab
ucalan <56287993+ucalan@users.noreply.github.com>
2020-12-25 11:28:12 +0800

    RBAC部分完工

---
+++ pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/mapper/RBACMapper.java
@@ -0,0 +1,18 @@
+package cn.edu.xjtu.stu.orangesoft.backdoor.mapper;
+
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Object;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Operation;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.PermissionConfig;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface RBACMapper {
+    List<PermissionConfig> GetPermissionConfigsByRoleID(@Param("RoleID")Integer RoleID);
+    PermissionConfig CheckPermission(@Param("Object")Object object,
+                                     @Param("Operation")Operation operation);
+}
