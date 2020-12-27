9aa5f70f34cb7ca40053a19522e874c1db34313b
1638821273 <76542938+1638821273@users.noreply.github.com>
2020-12-27 19:34:00 +0800

    new修改和Communication模块

---
+++ pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/service/CommunicationService.java
@@ -0,0 +1,41 @@
+package cn.edu.xjtu.stu.orangesoft.backdoor.service;
+
+import cn.edu.xjtu.stu.orangesoft.backdoor.core.DIUtil;
+import cn.edu.xjtu.stu.orangesoft.backdoor.mapper.CommunicationMapper;
+import cn.edu.xjtu.stu.orangesoft.backdoor.mapper.StudentMapper;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Communication;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.Operation;
+import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.ResultInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommunicationService {
+    @Autowired
+    CommunicationMapper communicationMapper;
+
+    @Autowired
+    StudentMapper studentmapper;
+
+    public List<String> GetCommunication(Integer TeamID) {
+        return communicationMapper.GetCommunication(TeamID);
+    }
+
+    public ResultInfo PostCommunication(Integer userID, Integer teamID, String context, Integer fileID) {
+        ResultInfo resultInfo = DIUtil.getBean(ResultInfo.class);
+        Communication communication = DIUtil.getBean(Communication.class);
+        communication.setCommunicationID(0);
+        communication.setUserID(userID);
+        communication.setTeamID(teamID);
+        communication.setContext(context);
+        communication.setFileID(fileID);
+        communicationMapper.PostCommunication(communication);
+        if (communication.getCommunicationID() != 0)
+            resultInfo.setResultInfo("成功！！");
+        else
+            resultInfo.setResultInfo("失败！！");
+        return resultInfo;
+    }
+}
