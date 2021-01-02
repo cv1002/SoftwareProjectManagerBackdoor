bda4a66ef398eb131da31828b1105940e0266eca
Kuroneko <74940958+Neroya@users.noreply.github.com>
2021-01-02 20:17:25 +0800

    解决剩余的接口传入后端pojo参数的问题

--- pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/controller/MilestoneController.java
+++ pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/controller/MilestoneController.java
@@ -79,28 +79,36 @@ public class MilestoneController {
     }
 
     /**
-     * 更改小组完成情况
+     * 更改小组项目进度
      *
-     * @param UserID                用户ID，用于RBAC
-     * @param UserPassword          用户密码，用于RBAC
-     * @param CompletionID          完成情况ID（原）
-     * @param ProjectCompletionInfo 新增完成情况
+     * @param UserID       用户ID，用于RBAC
+     * @param UserPassword 用户密码，用于RBAC
+     * @param CompletionID 进度信息ID
+     * @param FileID       进度代表文件ID
+     * @param TeamID       小组ID
+     * @param MilestoneID  进度代表里程碑ID
      * @return ResultInfo: {
-     * "resultInfo": String
-     * }
+     * * "resultInfo": String
+     * * }
      */
     @PutMapping(value = "/completion", produces = "application/json;charset=UTF-8")
     public String PutCompletion(@RequestParam("UserID") Integer UserID,
                                 @RequestParam("UserPassword") String UserPassword,
                                 @RequestParam("CompletionID") Integer CompletionID,
-                                @RequestParam("ProjectCompletion") String ProjectCompletionInfo) {
+                                @RequestParam("FileID") int FileID,
+                                @RequestParam("TeamID") int TeamID,
+                                @RequestParam("MilestoneID") int MilestoneID) {
         Objects objects = DIUtil.getBean(Objects.class);
         Operation operation = DIUtil.getBean(Operation.class);
         objects.setObjectName("completion");
         operation.setOperationDescription("PUT");
-        ProjectCompletion projectCompletion = gson.fromJson(ProjectCompletionInfo, ProjectCompletion.class);
         ResultInfo resultInfo;
         if (rbacService.CheckPermission(UserID, UserPassword, objects, operation)) {
+            ProjectCompletion projectCompletion = DIUtil.getBean(ProjectCompletion.class);
+            projectCompletion.setFileID(FileID);
+            projectCompletion.setCompletionID(CompletionID);
+            projectCompletion.setTeamID(TeamID);
+            projectCompletion.setMilestoneID(MilestoneID);
             resultInfo = milestoneService.PutCompletion(CompletionID, projectCompletion);
         } else {
             resultInfo = DIUtil.getBean(ResultInfo.class);
