ac6f8473fb1d9200086b916d0752a417f884c00a
Kuroneko <74940958+Neroya@users.noreply.github.com>
2021-01-02 12:07:29 +0800

    让最后登录信息无记录则新增

--- pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/mapper/LastLoginMapper.java
+++ pm-project-backdoor/src/main/java/cn/edu/xjtu/stu/orangesoft/backdoor/mapper/LastLoginMapper.java
@@ -2,11 +2,12 @@ package cn.edu.xjtu.stu.orangesoft.backdoor.mapper;
 
 import cn.edu.xjtu.stu.orangesoft.backdoor.pojo.LastLogin;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface LastLoginMapper {
     LastLogin GetLastLoginInfo(Integer UserID);
-    void refreshLastLogin(LastLogin lastLogin);
+    void refreshLastLogin(@Param("lastLogin") LastLogin lastLogin);
 }
